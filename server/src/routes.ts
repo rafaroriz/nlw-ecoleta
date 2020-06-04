e71da2367caa1f52172454683444809c1d60e098
Rafael Roriz <rafaelroriz.crz@gmail.com>
2020-06-04 00:24:04 -0300

    Created routes to populate database

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,60 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import knex from './database/connection'
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({message: 'Hello World!'})
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:4440/uploads/${item.image}`
+        };
+    });
+
+    return response.json(serializedItems)
 });
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    const insertedPoint = await trx('points').insert({
+        image: 'fake-image',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const point_id = insertedPoint[0];
+
+    const point_items = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        };
+    });
+
+    await trx('point_items').insert(point_items);
+
+    return response.json({success: true});
+})
 
 export default routes;
 
