ceafd49f7bcfd4af9cc0ccb854400bd44197fd9c
Rafael Roriz <rafaelroriz.crz@gmail.com>
2020-06-07 12:18:31 -0300

    Created mobile project with React Native and Expo

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#fff',
+  }
+});
