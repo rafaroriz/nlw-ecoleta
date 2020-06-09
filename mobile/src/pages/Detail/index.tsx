c3feba210850210cbac09dcc62ef1c3f20193a7f
Rafael Roriz <rafaelroriz.crz@gmail.com>
2020-06-09 10:44:31 -0300

    Integration with whatsapp and email apps

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import {Feather as Icon, FontAwesome} from '@expo/vector-icons';
 import {useNavigation, useRoute} from '@react-navigation/native';
-import {View, Image, Text, StyleSheet, TouchableOpacity, SafeAreaView} from 'react-native';
+import {View, Image, Text, StyleSheet, TouchableOpacity, SafeAreaView, Linking} from 'react-native';
 import Constants from 'expo-constants';
 import {RectButton} from 'react-native-gesture-handler';
 import api from '../../services/api'
+import * as MailComposer from 'expo-mail-composer';
 
 interface Params {
     point_id: number;
@@ -38,8 +39,19 @@ const Detail = () => {
         });
     }, []);
 
-    function handleNavigateBack () {
+    function handleNavigateBack() {
         navigation.goBack();
+    };
+
+    function handleComposeMail() {
+      MailComposer.composeAsync({
+        subject: 'Interesse na coleta de resíduos',
+        recipients: [data.point.email],
+      });
+    };
+
+    function handleWhatsapp() {
+      Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse sobre a coleta de resíduos`);
     };
 
     if (!data.point) {
@@ -65,12 +77,12 @@ const Detail = () => {
                 </View>
             </View>
             <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={() => {}}>
+                <RectButton style={styles.button} onPress={handleWhatsapp}>
                     <FontAwesome name="whatsapp" size={20} color="#fff" />
                     <Text style={styles.buttonText}>Whatsapp</Text>
                 </RectButton>
 
-                <RectButton style={styles.button} onPress={() => {}}>
+                <RectButton style={styles.button} onPress={handleComposeMail}>
                     <Icon name="mail" size={20} color="#fff" />
                     <Text style={styles.buttonText}>E-mail</Text>
                 </RectButton>
